1d82e59b275116e27edff228e1ed7f018f29cd82
zhelica <51505339+zhelica@users.noreply.github.com>
2021-08-12 14:43:09 +0800

    Add files via upload
    初始化

---
+++ hls_api/src/main/java/com/hls/api/hlsApplication.java
@@ -0,0 +1,15 @@
+package com.hls.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lizhe
+ * @date 2021-08-12 13:49
+ */
+@SpringBootApplication
+public class hlsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(hlsApplication.class,args);
+    }
+}
